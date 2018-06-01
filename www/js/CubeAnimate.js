82c0e7d8535c9989b5c2b71ecea63485a6a1289f
theDuodecim <juansebastian.012@gmail.com>
2018-06-01 02:53:40 +0000

    FirstCommit

---
+++ www/js/CubeAnimate.js
@@ -0,0 +1,71 @@
+var $$ = Dom7;
+
+window.cubeAnimation = $$(document).ready( function() {
+    // (selector).animate({styles},speed,easing,callback)
+    $$("#button1")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+
+    $$("#button2")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+    $$("#button3")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+    $$("#button4")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+    $$("#button5")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+    $$("#button6")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+   $$("#button7")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+    $$("#button8")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+    $$("#button9")
+        .delay(1000).animate({"background-color":"#1a00ff", "border-color":"#1a00ff"},1000,"linear").delay(1000) //propiedades, duracion, alijerar, llamada
+        .animate({"background-color":"#e2090a", "border-color":"#e2090a"},1000,"linear").delay(1000) // la segunda llamada de animate estara en espera hasta que la primera llamada sea ejecutada
+        .animate({"background-color":"#ffffff", "border-color":"#ffffff"},1000,"linear").delay(1000)
+        .animate({"background-color":"#ff5c0a", "border-color":"#ff5c0a"},1000,"linear").delay(1000)
+        .animate({"background-color":"#feff0d", "border-color":"#feff0d"},1000,"linear").delay(1000)
+        .animate({"background-color":"#35c908", "border-color":"#35c90d"},1000,"linear").delay(1000);
+
+
+});
