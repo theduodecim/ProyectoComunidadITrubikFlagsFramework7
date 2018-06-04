130f88a3031cf637925db064b8e7116775a848f1
theDuodecim <juansebastian.012@gmail.com>
2018-06-04 17:51:46 +0000

    FiveCommit Structure ready

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -14,59 +14,70 @@ var flagsConvinations = ["rgb", "rgo", "rgw", "rgy" , "rbw", "rbo", "gbw", "row"
  
 var flagsRgb = [
     "Agadir","Akiruno","Amazonas","Cukarica","Azerbaijan","Gabia","Bor","Belize",
-    "Eritrea","Mauritius","Namibia","Oman","South Africa", "Northern Province, Sri Lanka"
+    "Eritrea","Mauritius","Namibia","Oman","South Africa"
     ];
 var flagsRgw = [
     "Algeria","Żebbuġ-Ghawdex","Boyacá","Bauma","Birgu","Bulgaria","Equatorial Guinea"," Bangladesh naval Ensign",
-    "Somaliland","Santo Domingo","Heligoland","Groningen","Kuwait","Bulgaria Naval Ensign"
+    "Somaliland","Santo Domingo","Heligoland","Groningen","Kuwait","Bulgaria Naval Ensign","Northern Province, Sri Lanka","Mexico",
+    "Turkmenistan"
     ];
 var flagsRgo = [
-    "Sri Lanka","Jain flag", "Cusco","Zambia", "Ireland"
+    "Sri Lanka","Jain flag", "Cusco","Zambia", "Ireland","Zambia"
     ];
 var flagsRgy = [
     "Acre","Bolivia","Benin","Burkina Faso","Cameroon","Casanare","Congo","Ethiopia","Lithuania","Jagodina",
-    "Dominica","Grenada"
+    "Dominica","Grenada","Mauritania"
     ];
 var flagsRbw = [
     "Alagoas", "Attard","Australia","Puerto Rico","Russia","Schleswig-Holstein","Wyoming",
-    "Anguilla","Canada","Finland","Luxembourg","Taiwan"
+    "Anguilla","Canada","Finland","Luxembourg","Taiwan","South Korea"
     ];
 var flagsRbo = [
-    "Armenia","Bong","Arizona",
-    
+    "Armenia","Bong","Arizona","Swaziland"
     ];
 var flagsGbw = [
     "Australia","The Netherlands","Žužemberk","Azerbaijan", "Abruzzo", "Córdoba Colombia",
-    "Djibouti","Jastrowie"
+    "Djibouti","Jastrowie","Nicaragua","El Salvador","Guatemala"
     ];
 var flagsRow = [
-    "India","French Polynesia",""
-    
+    "India","French Polynesia","Armenia","Papua New Guinea"
     ];
 var flagsBow = [
     "Anguilla - Ensign", "Marshall Islands"
-   
     ];
 var flagsGow = [
-    "Cyprus",
+    "Cyprus","Niger"
     ];
 var flagsGbo = [
     "Karakalpakstan"
     ];
 var flagsRby = [
-    "Aragon",
+    "Aragon","Democratic Republic of the Congo",
     ];
-var flagsGby = [];
-var flagsOry = [];
-var flagsOgy = [];
-var flagsOby = [];
+var flagsGby = [
+    "Amapá","Brazil","Apure","Chocó","Dublin","Delta Amacuro","Bar","Christmas Island"
+    ];
+var flagsOry = [
+        "Los Angeles"
+    ];
+var flagsOgy = [
+    "Bhután",
+    ];
+var flagsOby = [
+    "Cojedes"
+    ];
 var flagsWry = [
-    "Aranđelovac", 
-    
+    "Aranđelovac","Vatican City","Brunei"
     ];
-var flagsWgy = [];
-var flagsWby = [];
-var flagsowy = [];
+var flagsWgy = [
+    "Bobrovec","Bormla","Ehime","Caquetá Department","Koryaks","Imavere Parish"
+    ];
+var flagsWby = [
+    "Argentina"
+    ];
+var flagsOwy = [
+     "Central African Republic"
+    ];
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
@@ -76,7 +87,7 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
  
-  $$("#button1").on("click", function(event) {
+  $$(".RubikTheflag").on("click", function(event) {
      console.log(event);
     var flagsConv = flagsConvinations.slice(0);
     var fRgb = flagsRgb.slice(0); 
@@ -93,7 +104,7 @@ $$(document).on('deviceready', function() {
     //check case 0
     switch(randomConvinationFunction()) {
              case 1:
-                 console.log("Hoalaa");
+                 console.log("RGB working");
                     $$("#flagbutton1").css(
                         "background-color", "red");
                          $$("#flagbutton2").css(
@@ -102,35 +113,61 @@ $$(document).on('deviceready', function() {
                          "background-color", "green")
                              function randomrgbFlagFunction() {
                                    var randomConvination = Math.floor((Math.random() * 2) + 1)
-                                   console.log("randomrgbisworkin");
+                                   console.log("random RGBflagWorking");
                                    return randomConvination;
                                 }
+                                if(randomrgbFlagFunction() != null) {
                                      switch(randomrgbFlagFunction()) {
                                         case 1:
-                                        $$("#flagname").text("Agadir");
+                                            console.log("rgbflag1");
+                                          flagName = $$("#flagname").text("Agadir");
                                        break; 
                                          case 2:
-                                          $$("#flagName").text("Akiruno");
+                                            console.log("rgbflag2");
+                                          flagName = $$("#flagname").text("Akiruno");
                                         break; 
-                                    
                                            }
+                                }
+                    
+                                    
             break;
             case 2:
-                 console.log("asdasdas");
+                 console.log("RGW Working");
                     $$("#flagbutton1").css(
                         "background-color", "red")
                          $$("#flagbutton2").css(
                          "background-color", "green")
                          $$("#flagbutton3").css(
                           "background-color", "white");
-                          
+                           function randomrgbFlagFunction() {
+                                   var randomConvination = Math.floor((Math.random() * 2) + 1)
+                                   console.log("random RGWflagWorking");
+                                   return randomConvination;
+                                }
+                                 if(randomrgbFlagFunction() != null) {
+                                     switch(randomrgbFlagFunction()) {
+                                        case 1:
+                                            console.log("rgbflag1");
+                                            flagName = $$("#flagname").text("Algeria");
+                                       break; 
+                                         case 2:
+                                            console.log("rgbflag2");
+                                            flagName = $$("#flagname").text("Żebbuġ-Ghawdex");
+                                        break; 
+                                           }
+                                }
                           
              break;
     };
-    
-    
-   
   })
+  
+  $$("#checkflagText").on("click", function(event) {
+      if(flagName != null) {
+      flagName =  $$("#flagname").text();
+       console.log(flagName);
+      }
+  })
+  
   
 });
 
@@ -148,13 +185,12 @@ $$(document).on('pageInit', function (e) {
     // Get page data from event data
     var page = e.detail.page;
 
-  if (page.name === 'page2') {
+  if (page.name === 'Page2') {
         // Following code will be executed for page with data-page attribute equal to "about"
        
+      $$("#flagNamePage2").text(flagName);
         
-        
-        
-        
+      
         
         
     }
