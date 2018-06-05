01a6b3cd956b4a1deab5dad2dc30d64a75f95a91
theDuodecim <juansebastian.012@gmail.com>
2018-06-05 11:16:27 +0000

    SixCommitAddingImg&Description

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -8,12 +8,32 @@ var $$ = Dom7;
 
 // VARIABLES GLOBALES
 var flagName = "";
+var flagDescription = "";
+var flagDescriptionText = "";
+var flagImage;
+
 
 var flagsConvinations = ["rgb", "rgo", "rgw", "rgy" , "rbw", "rbo", "gbw", "row", "bow", "gow",
  "gbo", "rby", "gby", "ory", "ogy", "oby", "wry", "wgy", "wby", "owy"];
  
+
+var AmazonasVenz = "El estado Amazonas, oficialmente Estado Indígena de Amazonas1​, es uno de los veintitrés estados que, junto con el Distrito Capital y" +
+"las Dependencias Federales, forman la República Bolivariana de Venezuela. Su capital es Puerto Ayacucho." +
+"Está ubicado al sur del país, en la región Guayana, " +
+"limitando al norte con Apure y Bolívar, al este y sur con Brasil mediante la divisoria de aguas de las cuencas del Orinoco y Amazonas, y al oeste con Colombia." +
+"Con 174 942 hab. en 2018 es el estado menos poblado, con 177 617 km², el segundo estado más extenso, por detrás de Bolívar, y con 1 habkm², el menos densamente poblado." +
+"Posee 7 municipios autónomos y 25 parroquias civiles. Sus principales ciudades son: Puerto Ayacucho, La Esmeralda, San Fernando de Atabapo y San Carlos de Río Negro.";                
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 var flagsRgb = [
-    "Agadir","Akiruno","Amazonas","Cukarica","Azerbaijan","Gabia","Bor","Belize",
+    "Akiruno","Amazonas (Venezuela)","Cukarica","Azerbaijan","Gabia","Bor","Belize",
     "Eritrea","Mauritius","Namibia","Oman","South Africa"
     ];
 var flagsRgw = [
@@ -22,7 +42,7 @@ var flagsRgw = [
     "Turkmenistan"
     ];
 var flagsRgo = [
-    "Sri Lanka","Jain flag", "Cusco","Zambia", "Ireland","Zambia"
+    "Sri Lanka", "Cusco","Zambia", "Ireland","Zambia"
     ];
 var flagsRgy = [
     "Acre","Bolivia","Benin","Burkina Faso","Cameroon","Casanare","Congo","Ethiopia","Lithuania","Jagodina",
@@ -55,7 +75,7 @@ var flagsRby = [
     "Aragon","Democratic Republic of the Congo",
     ];
 var flagsGby = [
-    "Amapá","Brazil","Apure","Chocó","Dublin","Delta Amacuro","Bar","Christmas Island"
+    "Amapá","Brazil","Apure","Chocó","Dublin","Delta Amacuro","Bar","Christmas Island","Agadir"
     ];
 var flagsOry = [
         "Los Angeles"
@@ -96,7 +116,7 @@ $$(document).on('deviceready', function() {
     
     
     function randomConvinationFunction() {
-    var randomConvination = Math.floor((Math.random() * 2) + 1)
+    var randomConvination = Math.floor((Math.random() * 3) + 1)
      console.log(randomConvination)
     return randomConvination;
    
@@ -120,7 +140,10 @@ $$(document).on('deviceready', function() {
                                      switch(randomrgbFlagFunction()) {
                                         case 1:
                                             console.log("rgbflag1");
-                                          flagName = $$("#flagname").text("Agadir");
+                                            flagName = $$("#flagname").text("Amazonas (Venezuela)");
+                                            flagDescription = $$("<p>").text(AmazonasVenz);
+                                            flagImage = $$("<img>").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Flag_of_Amazonas_State.svg/100px-Flag_of_Amazonas_State.svg.png");
+                                             console.log(flagDescription);
                                        break; 
                                          case 2:
                                             console.log("rgbflag2");
@@ -147,27 +170,65 @@ $$(document).on('deviceready', function() {
                                  if(randomrgbFlagFunction() != null) {
                                      switch(randomrgbFlagFunction()) {
                                         case 1:
-                                            console.log("rgbflag1");
+                                            console.log("rgwflag1");
                                             flagName = $$("#flagname").text("Algeria");
                                        break; 
                                          case 2:
-                                            console.log("rgbflag2");
+                                            console.log("rgwflag2");
                                             flagName = $$("#flagname").text("Żebbuġ-Ghawdex");
                                         break; 
                                            }
                                 }
                           
              break;
+             
+              case 3:
+                 console.log("RGW Working");
+                    $$("#flagbutton1").css(
+                        "background-color", "red")
+                         $$("#flagbutton2").css(
+                         "background-color", "green")
+                         $$("#flagbutton3").css(
+                          "background-color", "orange");
+                           function randomrgbFlagFunction() {
+                                   var randomConvination = Math.floor((Math.random() * 2) + 1)
+                                   console.log("random RGWflagWorking");
+                                   return randomConvination;
+                                }
+                                 if(randomrgbFlagFunction() != null) {
+                                     switch(randomrgbFlagFunction()) {
+                                        case 1:
+                                            console.log("rgoflag1");
+                                            flagName = $$("#flagname").text("Sri Lanka");
+                                       break; 
+                                         case 2:
+                                            console.log("rgoflag2");
+                                            flagName = $$("#flagname").text("Cusco");
+                                        break; 
+                                           }
+                                }
+                          
+             break;
+             
     };
+    
+    
+    
   })
-  
+  /* if(flagName == null) {*/
+      /* <button class="checkFlag" id="checkflagbutton"><a id="checkflagText" align="center" href="page2.html" >Explore <p id="flagname"></p> </a></button><br>*/
+    /*var exploreButton =  $$('<a style="background-color: red" id="checkflagText" align="center" href="page2.html">Explore</a>');
+    $$("#buttonexplorehere").append(exploreButton)*/
+    
+       
   $$("#checkflagText").on("click", function(event) {
-      if(flagName != null) {
       flagName =  $$("#flagname").text();
-       console.log(flagName);
-      }
+      flagDescriptionText = flagDescription.text();
+      console.log(flagImage);
+      console.log(flagDescriptionText);
+     /* appendTo("#textdescription");*/
   })
-  
+ /* }*/
   
 });
 
@@ -189,8 +250,8 @@ $$(document).on('pageInit', function (e) {
         // Following code will be executed for page with data-page attribute equal to "about"
        
       $$("#flagNamePage2").text(flagName);
-        
-      
+      $$("#textdescription").text(flagDescriptionText);
+      $$(flagImage).appendTo("#flagimg2");
         
         
     }
